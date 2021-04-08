093f9bb459079084b0533e5fecd4122d86515add
momoclouq <phamhoangminh050899@gmail.com>
2021-04-08 22:00:22 +0700

    Update header

--- scripts/mall_index.js
+++ scripts/mall_index.js
@@ -1,4 +1,13 @@
-function showMenu(){
-    let navbar = document.querySelector('nav');
-    navbar.classList.toggle('show');
-}
+let toggleBtn = document.querySelector(".toggle");
+toggleBtn.addEventListener("click", () => {
+    console.log("clicked");
+    let allItems = document.querySelectorAll(".item");
+
+    for (let i = 0; i < allItems.length; i++){
+        if (allItems[i].classList.contains("active")){
+            allItems[i].classList.remove("active");
+        } else {
+            allItems[i].classList.add("active");
+        }
+    }
+});
